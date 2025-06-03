b5c2dbe41e3a80693b14988a8d19c3ec2c12263d
Edson Amaya <edson_amaya99@live.com>
2025-06-02 21:40:23 -0600

    feat 2025-06-02 config enviroments

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,64 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 
-https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const environment = loadEnv(mode, process.cwd(), '');
 
+  console.log('👾 -----------------------------------------------------------👾');
+  console.log('👾 => file: vite.config.js:8 => defineConfig => mode:', mode);
+  console.log('👾 -----------------------------------------------------------👾');
 
+  console.log('👾 ----------------------------------------------------------👾');
+  console.log('👾 => file: vite.config.js:13 => defineConfig => env:', environment.VITE_API_URL);
+  console.log('👾 ----------------------------------------------------------👾');
+
+  return {
+    plugins: [react()],
+    base: environment.VITE_APP_URL,
+    server: {
+      host: true,
+    },
+    build: {
+      outDir: path.resolve(environment.VITE_APP_URL || 'dist'),
+      emptyOutDir: 0,
+      chunkSizeWarningLimit: 3000,
+    },
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, './src'),
+        },
+        {
+          find: '@assets',
+          replacement: path.resolve(__dirname, './src/assets'),
+        },
+        {
+          find: '@components',
+          replacement: path.resolve(__dirname, './src/components'),
+        },
+        {
+          find: '@services',
+          replacement: path.resolve(__dirname, './src/services'),
+        },
+        {
+          find: '@hooks',
+          replacement: path.resolve(__dirname, './src/hooks'),
+        },
+        {
+          find: '@utils',
+          replacement: path.resolve(__dirname, './src/utils'),
+        },
+        {
+          find: '@routes',
+          replacement: path.resolve(__dirname, './src/routes'),
+        },
+        {
+          find: '@theme',
+          replacement: path.resolve(__dirname, './src/theme'),
+        },
+      ],
+    },
+  };
+});
